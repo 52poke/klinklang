a3b52371d2aa072e833e3e8b0af9a32618d93ea0
Mudkip <i@mudkip.me>
2025-11-21 02:24:36 +0800

    chore: switch to sass-embedded

--- packages/klinklang-server/src/actions/scss.ts
+++ packages/klinklang-server/src/actions/scss.ts
@@ -1,4 +1,4 @@
-import sass from 'sass'
+import * as sass from 'sass-embedded'
 import { ActionWorker } from './base.ts'
 
 export interface SCSSActionInput {
@@ -17,7 +17,7 @@ export interface SCSSAction {
 }
 
 export class SCSSWorker extends ActionWorker<SCSSAction> {
-  public async process (): Promise<SCSSActionOutput> {
+  public async process(): Promise<SCSSActionOutput> {
     const variableText = Object.keys(this.input.variables).map(key => `$${key}: "${this.input.variables[key]}";\n`)
       .join('')
     const result = await sass.compileStringAsync(variableText + this.input.scss)
